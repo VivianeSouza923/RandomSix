c40ee7d443c9e48151b33f3993fe73da85e5e7bb
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-01-26 13:56:21 -0300

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,39 @@
-import { Component } from "react";
-
-type MyProps = {};
-type MyState = { produto: string};
-
-export default class App extends Component<MyProps, MyState> {
-
-  constructor(props: MyProps) {
-
-    super(props);
-
-    this.state = {
-
-      produto: "",
-    };
-
-    this.setProduto = this.setProduto.bind(this);
+import { FC, useState, useEffect, useLayoutEffect} from "react";
 
 
-  }
 
-  setProduto(produto: string) {
+const App: FC = () => {
 
-    this.setState({ produto })
+  const [produto, setProduto] = useState<string>("");
 
-  }
+  useEffect(() => {
+    setProduto("Thorin");
+    console.log("Cheguei no useEffect :>>");
+  }, []);
 
-  componentDidMount() {
+  useLayoutEffect(() => {
 
-    this.setProduto("Thorin");
+    console.log("Cheguei no useLayoutEffect :>> ");
+  }, []);
 
-  }
 
-  render() {
+  return (
+  
+    <div>
+      <input
+      
+        type="text"
+        placeholder="produto"
+        value={produto}
+        onChange={(event) => setProduto(event.target.value)}
+      
+      />
+      <h2>{produto}</h2>
 
-    return(
 
-      <div>
-
-          <input
-            type = "text"
-            placeholder = "produto"
-            value = {this.state.produto}
-            onChange = {(event) => this.setProduto(event.target.value)}
-
-          />
-          <h2>{this.state.produto}</h2>
-      </div>
+    </div>
+    
     );
-  }
 }
+
+export default App;
